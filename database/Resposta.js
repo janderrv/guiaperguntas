46686741dfaa8af6f1420999bccb5b1bdf0e0a55
Jander <janderrv26@gmail.com>
2020-01-28 02:10:55 -0400

    Criado model e post de resposta

---
+++ database/Resposta.js
@@ -0,0 +1,17 @@
+const Sequelize = require("sequelize");
+const connection = require("./database");
+
+//cria model
+const Resposta = connection.define("respostas", {
+    corpo: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    perguntaId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+});
+
+Resposta.sync({ force: false });
+module.exports = Resposta;
--- index.js
+++ index.js
@@ -8,45 +8,49 @@ const bodyParser = require("body-parser");
 const connection = require("./database/database");
 //importa model pergunta
 const Pergunta = require("./database/Pergunta");
+//importa model respota
+const Resposta = require("./database/Resposta");
 
 //database
 connection
     .authenticate()
-    .then(()=>{
+    .then(() => {
         console.log("conexao feita com o banco de dados");
     })
-    .catch((erro)=>{
+    .catch((erro) => {
         console.log(erro);
     });
 
 //configurar ejs como view engine
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 //usar arquivos estaticos
 app.use(express.static("public"));
 //usa bodyparser
-app.use(bodyParser.urlencoded({extended: false}));//ler via javascript
+app.use(bodyParser.urlencoded({ extended: false }));//ler via javascript
 app.use(bodyParser.json());//ler via json
 
 
 //rota da pagina inicial
-app.get("/", (req,res)=>{
-    Pergunta.findAll({row: true,order: [
-        ['id','DESC']
-    ]}).then(perguntas => {
-            //desenha o arquivo na tela
-        res.render("index",{
+app.get("/", (req, res) => {
+    Pergunta.findAll({
+        row: true, order: [
+            ['id', 'DESC']
+        ]
+    }).then(perguntas => {
+        //desenha o arquivo na tela
+        res.render("index", {
             perguntas: perguntas
         });
     });
 });
 
 //rota da pagina de pergunta
-app.get("/perguntar", (req,res)=>{
+app.get("/perguntar", (req, res) => {
     res.render("perguntar");
 })
 
 //rota do formulario de pergunta
-app.post("/salvarpergunta", (req,res)=>{
+app.post("/salvarpergunta", (req, res) => {
     var titulo = req.body.titulo;
     var descricao = req.body.descricao;
     //chama o model
@@ -54,30 +58,42 @@ app.post("/salvarpergunta", (req,res)=>{
         //faz o insert
         titulo: titulo,
         descricao: descricao
-    }).then(()=>{
+    }).then(() => {
         //redireciona para pagina inicial
         res.redirect("/");
     });
 });
 
 //rota pergunta por id
-app.get("/pergunta/:id", (req,res)=>{
+app.get("/pergunta/:id", (req, res) => {
     var id = req.params.id;
     Pergunta.findOne({
         //busca uma pergunta
-        where: {id: id}
-    }).then(pergunta =>{
-        if(pergunta != undefined){//pergunta encontrada
-            res.render("pergunta",{
+        where: { id: id }
+    }).then(pergunta => {
+        if (pergunta != undefined) {//pergunta encontrada
+            res.render("pergunta", {
                 pergunta: pergunta
             });
-        }else{//pergunta nao encontrada
+        } else {//pergunta nao encontrada
             res.redirect("/");
         }
     });
 });
 
+//rota formulario responder
+app.post("/responder", (req, res) => {
+    var corpo = req.body.corpo;
+    var perguntaId = req.body.pergunta;
+    Resposta.create({
+        corpo: corpo,
+        perguntaId: perguntaId
+    }).then(() => {
+        res.redirect("/pergunta/" + perguntaId);
+    });
+});
+
 //roda app
-app.listen(8000,()=>{
+app.listen(8000, () => {
     console.log("app rodando");
 });
